fe9a6fa94b4f687f07aa08d8630846d1fe6d69b3
Harsh Vardhan Dubey <118985672+HxrshVD@users.noreply.github.com>
2024-06-13 21:18:02 +0530

    Add files via upload

---
+++ Lecture-10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	int i,j,k;
+//	i=j=k=0;
+//	cout<<i<<" "<<j<<" "<<k<<endl;
+//	j=i++;
+//	cout<<i<<" "<<j<<" "<<k<<endl;
+//	k=++i;
+//	cout<<i<<" "<<j<<" "<<k<<endl;
+	int i=0;
+	for(i=1;i<=100;i++){
+		cout<<i<<endl;
+	}	
+	return 0;
+}
