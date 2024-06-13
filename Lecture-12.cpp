49107b7a5dd7bd247becb0f2e7ae657f960d60f9
Harsh Vardhan Dubey <118985672+HxrshVD@users.noreply.github.com>
2024-06-13 21:45:59 +0530

    Add files via upload

---
+++ Lecture-12.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	cout<<"Enter your password: ";
+//	int password;
+//	do{
+//		cin>>password;
+//	}
+//	while(password<999999);
+//	{
+//		cout<<"Enter your password: ";
+//		cin>>password;
+//	}
+	int i;
+	for(i=0; ;i++){
+		cout<<"Chocolate"<<endl;
+		if(i>100)
+		break;
+	}
+}
