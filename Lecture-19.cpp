660df776c5560ef814b5ef4d850f78ea643cad4a
Harsh Vardhan Dubey <118985672+HxrshVD@users.noreply.github.com>
2024-06-14 21:48:11 +0530

    Add files via upload

---
+++ Lecture-19.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    std::cout << "Size of char: " << sizeof(char) << " byte" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
+    std::cout << "Size of bool: " << sizeof(bool) << " byte" << std::endl;
+    std::cout << "Size of short: " << sizeof(short) << " bytes" << std::endl;
+    std::cout << "Size of long: " << sizeof(long) << " bytes" << std::endl;
+    std::cout << "Size of long long: " << sizeof(long long) << " bytes" << std::endl;
+    
+
+    return 0;
+}
